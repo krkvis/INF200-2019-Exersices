30fd64c2e009c753d3e07d2f6dd5a1d83fa02817
Kjersti <kjkv@nmbu.no>
2019-09-20 09:34:24 +0200

    Made minor changes to task C

--- src/kjersti_rustad_kvisberg_ex/ex02/bubble_sort.py
+++ src/kjersti_rustad_kvisberg_ex/ex02/bubble_sort.py
@@ -5,8 +5,8 @@ __email__ = "kjkv@nmbu.no"
 
 
 def bubble_sort(data):
-    """Sort list in increasing order using the Bubble Sort algorithm.
-    :param data: list or tuple with numbers.
+    """Sort list ord tuple in increasing order using the Bubble Sort algorithm.
+    :param data: list or tuple with numbers in random order.
     :returns: sorted list.
     """
     copy = list(data)
@@ -24,8 +24,8 @@ def bubble_sort(data):
 if __name__ == "__main__":
 
     for element in ((),
-                 (1,),
-                 (1, 3, 8, 12),
-                 (12, 8, 3, 1),
-                 (8, 3, 12, 1)):
+                    (1,),
+                    (1, 3, 8, 12),
+                    (12, 8, 3, 1),
+                    (8, 3, 12, 1)):
         print('{!s:>15} --> {!s:>15}'.format(element, bubble_sort(element)))
