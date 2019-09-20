46f79a780db22c7fb2c4b42b3e3293b2a8705144
Kjersti <kjkv@nmbu.no>
2019-09-20 09:17:59 +0200

    Answered task C

---
+++ src/kjersti_rustad_kvisberg_ex/ex02/bubble_sort.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Kjersti Rustad Kvisberg"
+__email__ = "kjkv@nmbu.no"
+
+
+def bubble_sort(data):
+    """Sort list in increasing order using the Bubble Sort algorithm.
+    :param data: list or tuple with numbers.
+    :returns: sorted list.
+    """
+    copy = list(data)
+    n = len(copy)
+    while n > 0:
+        i = 1
+        while i < n:
+            if copy[i] < copy[i - 1]:
+                copy[i], copy[i - 1] = copy[i - 1], copy[i]
+            i += 1
+        n -= 1
+    return copy
+
+
+if __name__ == "__main__":
+
+    for element in ((),
+                 (1,),
+                 (1, 3, 8, 12),
+                 (12, 8, 3, 1),
+                 (8, 3, 12, 1)):
+        print('{!s:>15} --> {!s:>15}'.format(element, bubble_sort(element)))
