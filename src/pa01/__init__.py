804e060f750cb803505ad5e3062529f11ba525f1
Kjersti <kjkv@nmbu.no>
2019-10-03 16:07:32 +0200

    Created __init__ file

---
+++ src/pa01/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Ida Lunde Naalsund, Kjersti Rustad Kvisberg"
+__email__ = "idna@nmbu.no, kjkv@nmbu.no"
+
+from .snakes_and_ladders import multi_game_experiment
