e6a060d460b2c7b8f846a50544e37b76166792b5
Kjersti <kjkv@nmbu.no>
2019-10-04 09:44:44 +0200

    Made changes to jupyter notebook file

--- src/pa01/chutes.py
+++ src/pa01/chutes.py
@@ -2,6 +2,8 @@
 # Regn
 # Samt sol
 
-import snakes_and_ladders
+from .snakes_and_ladders import check_position_on_ladder_or_snake, \
+    player_make_one_move, one_player_game, single_game, multiple_games, \
+    multi_game_experiment
 
 print(multi_game_experiment(100, 4, 87))
