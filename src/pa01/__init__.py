ab87cf2c691177ff71d3f00ceacb1b967a3c2072
Ida Lunde Naalsund <idna@nmbu.no>
2019-10-04 09:14:12 +0200

    Tried to fix import problem in notebook

--- src/pa01/__init__.py
+++ src/pa01/__init__.py
@@ -2,5 +2,3 @@
 
 __author__ = "Ida Lunde Naalsund, Kjersti Rustad Kvisberg"
 __email__ = "idna@nmbu.no, kjkv@nmbu.no"
-
-from .snakes_and_ladders import multi_game_experiment
--- src/pa01/chutes.py
+++ src/pa01/chutes.py
@@ -1,3 +1,7 @@
 # 03.10.2019
 # Regn
 # Samt sol
+
+import snakes_and_ladders
+
+print(multi_game_experiment(100, 4, 87))
