caab95fac3a8a64008753ad6496f7244bc259d4e
Kjersti <kjkv@nmbu.no>
2019-10-08 09:00:42 +0200

    Removed drafts from chutes.py file

--- src/pa01/chutes.py
+++ src/pa01/chutes.py
@@ -1,9 +1,4 @@
-# 03.10.2019
-# Regn
-# Samt sol
+# -*- coding: utf-8 -*-
 
-from .snakes_and_ladders import check_position_on_ladder_or_snake, \
-    player_make_one_move, one_player_game, single_game, multiple_games, \
-    multi_game_experiment
-
-print(multi_game_experiment(100, 4, 87))
+__author__ = "Ida Lunde Naalsund, Kjersti Rustad Kvisberg"
+__email__ = "idna@nmbu.no, kjkv@nmbu.no"
