6697a6e124daa41b765c18f152b58b58f6bdf74a
Kjersti <kjkv@nmbu.no>
2019-10-21 10:23:54 +0200

    Wrote init, move, is_at_home and get_position functions

--- src/kjersti_rustad_kvisberg_ex/ex04/walker.py
+++ src/kjersti_rustad_kvisberg_ex/ex04/walker.py
@@ -5,19 +5,28 @@ __email__ = "kjkv@nmbu.no"
 
 import random as rand
 
+
 class Walker:
-    def __init__(self):
-        pass
+    def __init__(self, x0, h):
+        self.h = h
+        self.x = x0
+        self.steps = 0
 
     def move(self):
-        pass
+        if rand.randint(0, 1) == 0:
+            self.x -= 1
+        else:
+            self.x += 1
+        self.steps += 1
 
     def is_at_home(self):
-        pass
+        return self.x == self.h
 
     def get_postition(self):
-        pass
+        return self.x
 
     def get_steps(self):
         pass
 
+    def waking_process(self):
+        pass
