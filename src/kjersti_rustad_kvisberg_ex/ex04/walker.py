6e8c82de06697a462afd01dc3846085b2ec08556
Kjersti <kjkv@nmbu.no>
2019-10-21 10:06:13 +0200

    Added Walker class, function shells and import

---
+++ src/kjersti_rustad_kvisberg_ex/ex04/walker.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Kjersti Rustad Kvisberg"
+__email__ = "kjkv@nmbu.no"
+
+import random as rand
+
+class Walker:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def is_at_home(self):
+        pass
+
+    def get_postition(self):
+        pass
+
+    def get_steps(self):
+        pass
+
