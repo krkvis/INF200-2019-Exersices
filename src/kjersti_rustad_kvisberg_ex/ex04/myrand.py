98c1d5c4a04c5caedd35225145ac510643c8f8f2
Kjersti <kjkv@nmbu.no>
2019-10-21 10:01:17 +0200

    Added docstrings to functions

--- src/kjersti_rustad_kvisberg_ex/ex04/myrand.py
+++ src/kjersti_rustad_kvisberg_ex/ex04/myrand.py
@@ -6,11 +6,28 @@ __email__ = "kjkv@nmbu.no"
 
 class LCGRand:
     def __init__(self, seed):
+        """
+        Initialises the class with given constants a and m.
+
+        Parameters
+        ----------
+        seed: int
+            The seed that the LCG bases the generating on.
+        """
         self.seed = seed
         self.a = 7**5
         self.m = 2**31 - 1
 
     def rand(self):
+        """
+        Uses a linear congruential generator to generate a random number based
+        on a seed given by user.
+
+        Returns
+        -------
+        random_number: int
+            The generated number.
+        """
         random_number = self.a * self.seed % self.m
         self.seed = random_number
         return random_number
@@ -18,10 +35,26 @@ class LCGRand:
 
 class ListRand:
     def __init__(self, list_of_numbers):
+        """
+        Initialises the class with list given by user and a counter.
+
+        Parameters
+        ----------
+        list_of_numbers: list
+            List of numbers that will be returned one by one as random numbers.
+        """
         self.numbers = list_of_numbers
         self.counter = 0
 
     def rand(self):
+        """
+        Returns random numbers from a list given by user.
+
+        Returns
+        -------
+        random_number: int
+            A random number drawn from the input list.
+        """
         if self.counter == len(self.numbers):
             raise RuntimeError
         else:
