bf30ff2458114435d73455625cd8c106ba8a43fa
Kjersti <kjkv@nmbu.no>
2019-10-21 08:50:10 +0200

    Wrote functions __init__ and rand in LCGRand class

--- src/kjersti_rustad_kvisberg_ex/ex04/myrand.py
+++ src/kjersti_rustad_kvisberg_ex/ex04/myrand.py
@@ -5,7 +5,9 @@ __email__ = "kjkv@nmbu.no"
 
 class LCGRand:
     def __init__(self, seed):
-
+        self.seed = seed
+        self.a = 7**5
+        self.m = 2**31 - 1
 
     def rand(self):
-
+        r[n+1] = self.a * r[n] mod self.m
