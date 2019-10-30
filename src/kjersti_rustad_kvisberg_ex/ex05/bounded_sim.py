3b478b2308c399e4a35ee53edce804adfedd3bc5
Kjersti <kjkv@nmbu.no>
2019-10-30 11:31:17 +0100

    Rewrote main block, single_walk, move and simulation init methods

--- src/kjersti_rustad_kvisberg_ex/ex05/bounded_sim.py
+++ src/kjersti_rustad_kvisberg_ex/ex05/bounded_sim.py
@@ -42,11 +42,11 @@ class BoundedWalker(Walker):
         if random.randint(0, 1) == 0:
             if self.position > self.left_limit:
                 self.position -= 1
-                self.steps += 1
+
         else:
             if self.position < self.right_limit:
                 self.position += 1
-                self.steps += 1
+        self.steps += 1
 
 
 class BoundedSimulation(Simulation):
@@ -70,15 +70,44 @@ class BoundedSimulation(Simulation):
         """
         self.start = start
         self.home = home
-        self.seed = random.seed(seed)
         self.left_limit = left_limit
         self.right_limit = right_limit
-        self.walker = BoundedWalker(start, home, left_limit, right_limit)
 
-        super().__init__(self.start, self.home, self.seed)
+        super().__init__(self.start, self.home, seed)
+
+    def single_walk(self):
+        """Simulate single walk from start to home, returning number of steps.
+
+        Returns
+        -------
+        int
+            The number of steps taken to reach home position.
+        """
+        bounded_walker = BoundedWalker(
+            self.start, self.home, self.left_limit, self.right_limit
+        )
+        while not bounded_walker.is_at_home():
+            bounded_walker.move()
+        return bounded_walker.get_steps()
 
 
 if __name__ == '__main__':
-    walking_person = BoundedWalker(0, 5, -1000, 1000)
-    position = walking_person.get_position()
-    print(position, walking_person.steps)
+    num_sim = 20
+    start_pos = 0
+    home_pos = 20
+    seed_val = 1
+    left_limits = [0, -10, -100, -1000, -10000]
+    print(
+        f'Start position: {start_pos}, '
+        f'Home position: {home_pos}, '
+        f'Seed value: {seed_val}, '
+        f'Right boundary: {home_pos}, '
+    )
+    for limit in left_limits:
+        bounded_walker_sim = BoundedSimulation(
+            start_pos, home_pos, seed_val, limit, home_pos
+        )
+        print(
+            f'Left boundary: {limit}\n'
+            f'Lengths of walks: {bounded_walker_sim.run_simulation(num_sim)}'
+        )
