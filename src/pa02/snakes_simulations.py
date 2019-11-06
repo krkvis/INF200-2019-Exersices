07ebdbceb5ecf165054ea1345280b15bad8efa83
Kjersti <kjkv@nmbu.no>
2019-11-06 15:04:38 +0100

    Created class shells with empty methods

--- src/pa02/snakes_simulations.py
+++ src/pa02/snakes_simulations.py
@@ -2,3 +2,64 @@
 
 __author__ = "Ida Lunde Naalsund, Kjersti Rustad Kvisberg"
 __email__ = "idna@nmbu.no, kjkv@nmbu.no"
+
+class Board:
+    def __init__(self):
+        pass
+
+    def goal_reached(self):
+        pass
+
+    def position_adjustment(self):
+        pass
+
+
+class Player:
+    def __init__(self, board):
+        pass
+
+    def move(self):
+        pass
+
+
+class ResilientPlayer(Player):
+    def __init__(self):
+        super().__init__(self, board)
+        pass
+
+    def move(self):
+        pass
+
+
+class LazyPlayer(Player):
+    def __init__(self):
+        super().__init__(self, board)
+        pass
+
+    def move(self):
+        pass
+
+
+class Simulation:
+    def __init__(self, board, seed, bool_flag, players):
+        pass
+
+    def single_game(self):
+        pass
+
+    def run_simulation(self, num_games):
+        pass
+
+    def get_results(self):
+        pass
+
+    def winner_per_type(self):
+        pass
+
+    def durations_per_type(self):
+        pass
+
+    def players_per_type(self):
+        pass
+
+    
