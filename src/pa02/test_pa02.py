9350fdc804fd718a8f07706745a99df0ddc1b97c
Kjersti <kjkv@nmbu.no>
2019-11-07 14:18:47 +0100

    Added additional test file

---
+++ src/pa02/test_pa02.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Additional set of compatibility tests for PA02.
+"""
+
+__author__ = "Ida Lunde Naalsund, Kjersti Rustad Kvisberg"
+__email__ = "idna@nmbu.no, kjkv@nmbu.no"
+
+import snakes_simulations as ss
